f8861b57174defc57ec2343f140d287592ad133b
Josh Smith <josh@joshsmith.codes>
2020-04-14 00:10:18 -0700

    Add model transitions

--- src/renderer/components/Modal.js
+++ src/renderer/components/Modal.js
@@ -1,30 +1,29 @@
 import React from 'react'
 
 export default function Modal (props) {
-  const coverClass = props.open
-    ? 'modal-cover modal-cover-active'
-    : 'modal-cover'
-  const containerClass = props.open
-    ? 'modal-container modal-container-active'
-    : 'modal-container'
+  const modalClass = props.open
+    ? 'modal modal-active'
+    : 'modal'
   return (
-    <div>
-      <div className={containerClass} style={{ ...props.style }}>
-        {!!props.onClose && (
-          <a className='ion-ios-close cancel' onClick={props.onClose} />
-        )}
-        <div className='modal-header'>{props.header}</div>
-        <div className='modal-body'>{props.body}</div>
-        {!!props.message && !!props.message.text && (
-          <p className={props.message.error ? 'error-message' : 'message'}>
-            {props.message.text}
-          </p>
-        )}
-        {!!props.action && <div className='modal-action'>{props.action}</div>}
+    <div className={modalClass}>
+      <div className="modal-outer-container">
+        <div className="modal-inner-container" style={{ ...props.style }}>
+          {!!props.onClose && (
+            <a className='ion-ios-close cancel' onClick={props.onClose} />
+          )}
+          <div className='modal-header'>{props.header}</div>
+          <div className='modal-body'>{props.body}</div>
+          {!!props.message && !!props.message.text && (
+            <p className={props.message.error ? 'error-message' : 'message'}>
+              {props.message.text}
+            </p>
+          )}
+          {!!props.action && <div className='modal-action'>{props.action}</div>}
+        </div>
       </div>
 
       <div
-        className={coverClass}
+        className="modal-cover"
         onClick={props.onClose ? props.onClose : undefined}
       ></div>
     </div>
